4c5a33be92d29ffba236aa7e99e8c93e9a6985b3
Pablo <pabloa@cabify.com>
2016-09-13 16:42:14 +0200

    Added zIndex and iconView properties to gem. Added a param to init an annotation for googlemaps, by default

--- lib/project/annotation.rb
+++ lib/project/annotation.rb
@@ -22,7 +22,10 @@ class MapAnnotation
                 :info_contents_view,
                 :info_window_anchor,
                 :ground_anchor,
-                :tappable
+                :tappable,
+                :zIndex,
+                :iconView,
+                :marker
 
   def initialize(opts = {})
     @point = Point.new
@@ -42,45 +45,61 @@ class MapAnnotation
   def setCoordinate(coordinate)
     willChangeValueForKey("coordinate")
     self.point.set(coordinate)
-    marker.position = point.asCLPoint if @GMSMarker
+    self.marker.position = point.asCLPoint if @marker
     didChangeValueForKey("coordinate")
   end
 
   def point=(point)
     self.point.set(point)
-    setCoordinate(point) if @GMSMarker
+    setCoordinate(point) if @marker
     self.point
   end
 
   def marker
-    @GMSMarker ||= begin
-      _marker = GMSMarker.markerWithPosition(coordinate)
-      _marker.snippet = self.subtitle
-      _marker.title = self.title
-      _marker.appearAnimation = KGMSMarkerAnimationPop if self.animated
-      _marker.icon = self.image
-      _marker.position = point.asCLPoint
-      if self.image_url
-        url = NSURL.URLWithString(image_url)
-        SDWebImageManager.sharedManager.downloadWithURL(url, options:SDWebImageRetryFailed,
-          progress:nil, completed: proc do |image, error, cached, finished|
+    @marker ||=
+      begin
+        _marker = GMSMarker.markerWithPosition(coordinate)
+        _marker.snippet = self.subtitle
+        _marker.title = self.title
+        _marker.appearAnimation = KGMSMarkerAnimationPop if self.animated
+        _marker.icon = self.image
+        _marker.position = point.asCLPoint
+        _marker.zIndex = self.zIndex if self.zIndex
+        _marker.iconView = self.iconView if self.iconView
+        if self.image_url
+          url = NSURL.URLWithString(image_url)
+          SDWebImageManager.sharedManager.downloadWithURL(url, options:SDWebImageRetryFailed,
+                                                          progress:nil, completed: proc do |image, error, cached, finished|
             _marker.icon = image if finished
           end)
+        end
+        _marker.infoWindowAnchor = self.info_window_anchor if self.info_window_anchor
+        _marker.groundAnchor = self.ground_anchor if self.ground_anchor
+        _marker.tappable = self.tappable.nil? ? true : self.tappable
+        _marker
       end
-      _marker.infoWindowAnchor = self.info_window_anchor if self.info_window_anchor
-      _marker.groundAnchor = self.ground_anchor if self.ground_anchor
-      _marker.tappable = self.tappable.nil? ? true : self.tappable
-      _marker
-    end
   end
 
   def image=(image)
-    marker.icon = image if @GMSMarker
+    self.marker.icon = image if @marker
     @image = image
   end
 
+  def zIndex=(zIndex)
+    @zIndex = zIndex
+    self.marker.zIndex = zIndex if @marker
+    self.zIndex
+  end
+
+  def iconView=(iconView)
+    @iconView = iconView
+    self.marker.iconView = iconView if @marker
+    self.iconView
+  end
+
+  # Google maps only
   def mapView=(mapView)
-    marker.map = mapView
+    self.marker.map = mapView
   end
 
 end
