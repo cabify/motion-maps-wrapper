93c2ceba365bb16ee4f476d60bb4997d59100ac1
Mark Villacampa <m@markvillacampa.com>
2016-09-28 03:09:38 +0200

    Rename the project folder so individual files can be required from CRuby like:
    require 'motion-maps-wrapper/point'

--- lib/motion-maps-wrapper.rb
+++ lib/motion-maps-wrapper.rb
@@ -7,7 +7,7 @@ end
 
 lib_dir_path = File.dirname(File.expand_path(__FILE__))
 Motion::Project::App.setup do |app|
-  app.files.unshift(Dir.glob(File.join(lib_dir_path, "project/**/*.rb")))
+  app.files.unshift(Dir.glob(File.join(lib_dir_path, "motion-maps-wrapper/**/*.rb")))
 
   app.frameworks << 'MapKit'
 end
